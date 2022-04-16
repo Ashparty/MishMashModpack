2865d690068ebae6faa638e29d3ce35f077468f7
PukoPotPie <colle@10.0.0.21>
2022-04-15 19:32:16 -0700

    TwitchStuffYAY

--- src/main/java/net/mcreator/mishmashed/item/TwitchyArmorItem.java
+++ src/main/java/net/mcreator/mishmashed/item/TwitchyArmorItem.java
@@ -1,10 +1,25 @@
 
 package net.mcreator.mishmashed.item;
 
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.resources.ResourceLocation;
+
+import net.mcreator.mishmashed.procedures.TwitchyArmorHelmetTickEventProcedure;
+import net.mcreator.mishmashed.init.MishmashedModTabs;
+import net.mcreator.mishmashed.init.MishmashedModItems;
 
 public abstract class TwitchyArmorItem extends ArmorItem {
-
 	public TwitchyArmorItem(EquipmentSlot slot, Item.Properties properties) {
 		super(new ArmorMaterial() {
 			@Override
@@ -29,7 +44,7 @@ public abstract class TwitchyArmorItem extends ArmorItem {
 
 			@Override
 			public Ingredient getRepairIngredient() {
-				return Ingredient.of(new ItemStack(MishmashedModItems.DELETED_MOD_ELEMENT));
+				return Ingredient.of(new ItemStack(MishmashedModItems.TWITCH));
 			}
 
 			@Override
@@ -50,63 +65,55 @@ public abstract class TwitchyArmorItem extends ArmorItem {
 	}
 
 	public static class Helmet extends TwitchyArmorItem {
-
 		public Helmet() {
 			super(EquipmentSlot.HEAD, new Item.Properties().tab(MishmashedModTabs.TAB_WHAT).fireResistant());
-
 			setRegistryName("twitchy_armor_helmet");
 		}
 
 		@Override
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-			return "mishmashed:textures/models/armor/twitchy_layer_1.png";
+			return "mishmashed:textures/models/armor/layer_1twitch_layer_1.png";
 		}
 
+		@Override
+		public void onArmorTick(ItemStack itemstack, Level world, Player entity) {
+			TwitchyArmorHelmetTickEventProcedure.execute(entity);
+		}
 	}
 
 	public static class Chestplate extends TwitchyArmorItem {
-
 		public Chestplate() {
 			super(EquipmentSlot.CHEST, new Item.Properties().tab(MishmashedModTabs.TAB_WHAT).fireResistant());
-
 			setRegistryName("twitchy_armor_chestplate");
 		}
 
 		@Override
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-			return "mishmashed:textures/models/armor/twitchy_layer_1.png";
+			return "mishmashed:textures/models/armor/layer_1twitch_layer_1.png";
 		}
-
 	}
 
 	public static class Leggings extends TwitchyArmorItem {
-
 		public Leggings() {
 			super(EquipmentSlot.LEGS, new Item.Properties().tab(MishmashedModTabs.TAB_WHAT).fireResistant());
-
 			setRegistryName("twitchy_armor_leggings");
 		}
 
 		@Override
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-			return "mishmashed:textures/models/armor/twitchy_layer_2.png";
+			return "mishmashed:textures/models/armor/layer_1twitch_layer_2.png";
 		}
-
 	}
 
 	public static class Boots extends TwitchyArmorItem {
-
 		public Boots() {
 			super(EquipmentSlot.FEET, new Item.Properties().tab(MishmashedModTabs.TAB_WHAT).fireResistant());
-
 			setRegistryName("twitchy_armor_boots");
 		}
 
 		@Override
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-			return "mishmashed:textures/models/armor/twitchy_layer_1.png";
+			return "mishmashed:textures/models/armor/layer_1twitch_layer_1.png";
 		}
-
 	}
-
 }
